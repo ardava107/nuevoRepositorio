3dcfe114107eaa05250a4a5e76de5f277cc55d6b
midominio.com <you@example.com>
2021-12-05 23:28:14 -0300

    initial commit

---
+++ catalogo/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['midominio.com.ar']
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    #BASE DE DATO DE PRODUCCION
+}
